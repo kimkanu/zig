3e1d9915ce4239437505a502128e55defc957288
Keonwoo Kim <me@kanu.kim>
2022-08-20 02:07:52 +0900

    add a hello world project

---
+++ hello-world/src/main.zig
@@ -0,0 +1,22 @@
+const std = @import("std");
+const Table = @import("table-helper").Table;
+
+pub fn main() !void {
+    try std.io.getStdOut().writer().print("{}\n", .{
+        Table(&[_][]const u8{ "Version", "Date" }){
+            .data = &[_][2][]const u8{
+                .{ "0.7.1", "2020-12-13" },
+                .{ "0.7.0", "2020-11-08" },
+                .{ "0.6.0", "2020-04-13" },
+                .{ "0.5.0", "2019-09-30" },
+            },
+        },
+    });
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
